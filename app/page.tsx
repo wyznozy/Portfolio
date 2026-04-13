224bec2007442b0a14bb80654f9ba7c18dc23048
Michael <mchaudev@gmail.com>
2026-04-13 18:33:58 -0500

    Removed buttons

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ import { motion } from "framer-motion"
 function ProfileCircle() {
   return (
     <motion.div
-      className="flex flex-col items-center"
+      className="flex flex-col items-center gap-5"
       initial={{ y: "-100vh", opacity: 0 }}
       animate={{ y: 0, opacity: 1 }}
       transition={{ type: "spring", stiffness: 60, damping: 18, restSpeed: 0.5, restDelta: 0.01, delay: 0.1 }}
@@ -15,9 +15,10 @@ function ProfileCircle() {
       <div className="w-75 h-75 rounded-full overflow-hidden shadow-xl ring-4 ring-white dark:ring-zinc-800">
         <Image src="/profile.jpg" alt="Profile" width={300} height={300} className="object-cover w-full h-full" />
       </div>
-      <h1 className="text-4xl font-semibold tracking-tight text-black dark:text-white">
-        About Me. blah blah blah
-      </h1>
+      <div className="flex flex-col items-center mt-1">
+        <h1 className="text-4xl font-bold tracking-tight text-black dark:text-white">Michael Chau</h1>
+        <h2 className="text-3xl">Full Stack Software Engineer</h2>
+      </div>
     </motion.div>
   );
 }
@@ -25,10 +26,10 @@ function ProfileCircle() {
 function ContentSquare(){
   return (
     <motion.div
-    className="flex flex-col items-center gap-4"
-    initial={{ y: "100vh", opacity: 0 }}
-    animate={{ y: 0, opacity: 1 }}
-    transition={{ type: "spring", stiffness: 60, damping: 18, restSpeed: 0.5, restDelta: 0.01, delay: 0.1 }}>
+      className="flex flex-col items-center gap-4"
+      initial={{ y: "100vh", opacity: 0 }}
+      animate={{ y: 0, opacity: 1 }}
+      transition={{ type: "spring", stiffness: 60, damping: 18, restSpeed: 0.5, restDelta: 0.01, delay: 0.1 }}>
 
 
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
@@ -57,6 +58,187 @@ function ContentSquare(){
 
     </motion.div>
   );
+}
+
+function Content() {
+    return (
+        <>
+            <article
+                style={{
+                    maxWidth: 500,
+                    padding: "150px 20px",
+                    display: "flex",
+                    flexDirection: "column",
+                    gap: 20,
+                }}
+            >
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Aliquam ac rhoncus quam.
+                </p>
+                <p>
+                    Fringilla quam urna. Cras turpis elit, euismod eget ligula
+                    quis, imperdiet sagittis justo. In viverra fermentum ex ac
+                    vestibulum. Aliquam eleifend nunc a luctus porta. Mauris
+                    laoreet augue ut felis blandit, at iaculis odio ultrices.
+                    Nulla facilisi. Vestibulum cursus ipsum tellus, eu tincidunt
+                    neque tincidunt a.
+                </p>
+                <h2>Sub-header</h2>
+                <p>
+                    In eget sodales arcu, consectetur efficitur metus. Duis
+                    efficitur tincidunt odio, sit amet laoreet massa fringilla
+                    eu.
+                </p>
+                <p>
+                    Pellentesque id lacus pulvinar elit pulvinar pretium ac non
+                    urna. Mauris id mauris vel arcu commodo venenatis. Aliquam
+                    eu risus arcu. Proin sit amet lacus mollis, semper massa ut,
+                    rutrum mi.
+                </p>
+                <p>
+                    Sed sem nisi, luctus consequat ligula in, congue sodales
+                    nisl.
+                </p>
+                <p>
+                    Vestibulum bibendum at erat sit amet pulvinar. Pellentesque
+                    pharetra leo vitae tristique rutrum. Donec ut volutpat ante,
+                    ut suscipit leo.
+                </p>
+                <h2>Sub-header</h2>
+                <p>
+                    Maecenas quis elementum nulla, in lacinia nisl. Ut rutrum
+                    fringilla aliquet. Pellentesque auctor vehicula malesuada.
+                    Aliquam id feugiat sem, sit amet tempor nulla. Quisque
+                    fermentum felis faucibus, vehicula metus ac, interdum nibh.
+                    Curabitur vitae convallis ligula. Integer ac enim vel felis
+                    pharetra laoreet. Interdum et malesuada fames ac ante ipsum
+                    primis in faucibus. Pellentesque hendrerit ac augue quis
+                    pretium.
+                </p>
+                <p>
+                    Morbi ut scelerisque nibh. Integer auctor, massa non dictum
+                    tristique, elit metus efficitur elit, ac pretium sapien nisl
+                    nec ante. In et ex ultricies, mollis mi in, euismod dolor.
+                </p>
+                <p>Quisque convallis ligula non magna efficitur tincidunt.</p>
+                <p>
+                    Pellentesque id lacus pulvinar elit pulvinar pretium ac non
+                    urna. Mauris id mauris vel arcu commodo venenatis. Aliquam
+                    eu risus arcu. Proin sit amet lacus mollis, semper massa ut,
+                    rutrum mi.
+                </p>
+                <p>
+                    Sed sem nisi, luctus consequat ligula in, congue sodales
+                    nisl.
+                </p>
+                <p>
+                    Vestibulum bibendum at erat sit amet pulvinar. Pellentesque
+                    pharetra leo vitae tristique rutrum. Donec ut volutpat ante,
+                    ut suscipit leo.
+                </p>
+                <h2>Sub-header</h2>
+                <p>
+                    Maecenas quis elementum nulla, in lacinia nisl. Ut rutrum
+                    fringilla aliquet. Pellentesque auctor vehicula malesuada.
+                    Aliquam id feugiat sem, sit amet tempor nulla. Quisque
+                    fermentum felis faucibus, vehicula metus ac, interdum nibh.
+                    Curabitur vitae convallis ligula. Integer ac enim vel felis
+                    pharetra laoreet. Interdum et malesuada fames ac ante ipsum
+                    primis in faucibus. Pellentesque hendrerit ac augue quis
+                    pretium.
+                </p>
+                <p>
+                    Morbi ut scelerisque nibh. Integer auctor, massa non dictum
+                    tristique, elit metus efficitur elit, ac pretium sapien nisl
+                    nec ante. In et ex ultricies, mollis mi in, euismod dolor.
+                </p>
+                <p>Quisque convallis ligula non magna efficitur tincidunt.</p>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Aliquam ac rhoncus quam.
+                </p>
+                <p>
+                    Fringilla quam urna. Cras turpis elit, euismod eget ligula
+                    quis, imperdiet sagittis justo. In viverra fermentum ex ac
+                    vestibulum. Aliquam eleifend nunc a luctus porta. Mauris
+                    laoreet augue ut felis blandit, at iaculis odio ultrices.
+                    Nulla facilisi. Vestibulum cursus ipsum tellus, eu tincidunt
+                    neque tincidunt a.
+                </p>
+                <h2>Sub-header</h2>
+                <p>
+                    In eget sodales arcu, consectetur efficitur metus. Duis
+                    efficitur tincidunt odio, sit amet laoreet massa fringilla
+                    eu.
+                </p>
+                <p>
+                    Pellentesque id lacus pulvinar elit pulvinar pretium ac non
+                    urna. Mauris id mauris vel arcu commodo venenatis. Aliquam
+                    eu risus arcu. Proin sit amet lacus mollis, semper massa ut,
+                    rutrum mi.
+                </p>
+                <p>
+                    Sed sem nisi, luctus consequat ligula in, congue sodales
+                    nisl.
+                </p>
+                <p>
+                    Vestibulum bibendum at erat sit amet pulvinar. Pellentesque
+                    pharetra leo vitae tristique rutrum. Donec ut volutpat ante,
+                    ut suscipit leo.
+                </p>
+                <h2>Sub-header</h2>
+                <p>
+                    Maecenas quis elementum nulla, in lacinia nisl. Ut rutrum
+                    fringilla aliquet. Pellentesque auctor vehicula malesuada.
+                    Aliquam id feugiat sem, sit amet tempor nulla. Quisque
+                    fermentum felis faucibus, vehicula metus ac, interdum nibh.
+                    Curabitur vitae convallis ligula. Integer ac enim vel felis
+                    pharetra laoreet. Interdum et malesuada fames ac ante ipsum
+                    primis in faucibus. Pellentesque hendrerit ac augue quis
+                    pretium.
+                </p>
+                <p>
+                    Morbi ut scelerisque nibh. Integer auctor, massa non dictum
+                    tristique, elit metus efficitur elit, ac pretium sapien nisl
+                    nec ante. In et ex ultricies, mollis mi in, euismod dolor.
+                </p>
+                <p>Quisque convallis ligula non magna efficitur tincidunt.</p>
+                <p>
+                    Pellentesque id lacus pulvinar elit pulvinar pretium ac non
+                    urna. Mauris id mauris vel arcu commodo venenatis. Aliquam
+                    eu risus arcu. Proin sit amet lacus mollis, semper massa ut,
+                    rutrum mi.
+                </p>
+                <p>
+                    Sed sem nisi, luctus consequat ligula in, congue sodales
+                    nisl.
+                </p>
+                <p>
+                    Vestibulum bibendum at erat sit amet pulvinar. Pellentesque
+                    pharetra leo vitae tristique rutrum. Donec ut volutpat ante,
+                    ut suscipit leo.
+                </p>
+                <h2>Sub-header</h2>
+                <p>
+                    Maecenas quis elementum nulla, in lacinia nisl. Ut rutrum
+                    fringilla aliquet. Pellentesque auctor vehicula malesuada.
+                    Aliquam id feugiat sem, sit amet tempor nulla. Quisque
+                    fermentum felis faucibus, vehicula metus ac, interdum nibh.
+                    Curabitur vitae convallis ligula. Integer ac enim vel felis
+                    pharetra laoreet. Interdum et malesuada fames ac ante ipsum
+                    primis in faucibus. Pellentesque hendrerit ac augue quis
+                    pretium.
+                </p>
+                <p>
+                    Morbi ut scelerisque nibh. Integer auctor, massa non dictum
+                    tristique, elit metus efficitur elit, ac pretium sapien nisl
+                    nec ante. In et ex ultricies, mollis mi in, euismod dolor.
+                </p>
+                <p>Quisque convallis ligula non magna efficitur tincidunt.</p>
+            </article>
+        </>
+    )
 }
 
 function Button() {
@@ -88,7 +270,7 @@ export default function Home() {
     <div className="flex flex-col items-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex w-full max-w-3xl flex-col items-center gap-8 py-32 px-16 bg-white dark:bg-black sm:items-start">
         <ProfileCircle />
-        <Image
+        {/* <Image
           className="dark:invert"
           src="/next.svg"
           alt="Next.js logo"
@@ -117,12 +299,26 @@ export default function Home() {
             className="w-16 h-16 bg-blue-500"
         />
 
-        <Button />
+        <Button /> */}
+
+      <motion.h1
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ duration: 3 }}
+      >
+        <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
+          Welcome to my personal website! :)
+
+
+        </p>
+      </motion.h1>
+
+
 
         <ContentSquare/>
 
 
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
+        {/* <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
           <a
             className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
@@ -146,7 +342,7 @@ export default function Home() {
           >
             Documentation
           </a>
-        </div>
+        </div> */}
       </main>
     </div>
   );
