08531e6b947964f1785f900cc27b09d602d06c8d
Michael <mchaudev@gmail.com>
2026-04-14 00:49:59 -0500

    updated links to social

--- app/page.tsx
+++ app/page.tsx
@@ -32,16 +32,22 @@ function ContentSquare() {
       animate={{ y: 0, opacity: 1 }}
       transition={{ type: "spring", stiffness: 60, damping: 18, restSpeed: 0.5, restDelta: 0.01, delay: 0.1 }}
     >
-      <div className="flex flex-row gap-50 items-center justify-between text-center sm:items-start sm:text-left">
-        <Image src={`${process.env.NEXT_PUBLIC_BASE_PATH ?? ""}/linkedin.png`} alt="LinkedIn" width={40} height={40} className="object-contain w-16 h-16" />
-        <Image src={`${process.env.NEXT_PUBLIC_BASE_PATH ?? ""}/github.png`} alt="LinkedIn" width={40} height={40} className="object-contain w-16 h-16" />
+      <div className="flex flex-row w-full items-center justify-between text-center">
+
+        <a className="inline-flex" href="https://www.linkedin.com/in/michaelanhchau/" target="_blank" rel="noopener noreferrer">
+          <Image src={`${process.env.NEXT_PUBLIC_BASE_PATH ?? ""}/linkedin.png`} alt="LinkedIn" width={40} height={40} className="object-contain w-12 h-12" />
+        </a>
+
+        <a  className="inline-flex" href="https://github.com/wyznozy" target="_blank" rel="noopener noreferrer">
+          <Image src={`${process.env.NEXT_PUBLIC_BASE_PATH ?? ""}/github.png`} alt="GitHub" width={40} height={40} className="object-contain w-12 h-12" />
+        </a>
       </div>
     </motion.div>
   );
 }
 
 const navItems = [
-  { number: "01", label: "HOME" },
+  { number: "01", label: "ABOUT ME" },
   { number: "02", label: "PHOTOS" },
   { number: "03", label: "CONTACT" },
 ]
